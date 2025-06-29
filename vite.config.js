7d0d654168c0f09abcf77d0a9b95cf1b8d413f58
Daniel Leitner <111922763+Jesuslovesshiva@users.noreply.github.com>
2025-06-29 02:32:45 +0200

    Add dist folder for GitHub Pages deployment from main branch

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    assetsInclude: ['**/*.glb'],
-    build: {
-      outDir: 'dist',
-      assetsDir: 'assets',
-      sourcemap: false,
-    },
-  }
-
-  if (command !== 'serve') {
-    config.base = '/3D_portfolio-main/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
+  assetsInclude: ['**/*.glb'],
+  base: '/3D_portfolio-main/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+  },
 })
