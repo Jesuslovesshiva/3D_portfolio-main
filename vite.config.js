56350acdba1bbc868dc0fb4470106d4a5f974c65
Daniel Leitner <111922763+Jesuslovesshiva@users.noreply.github.com>
2025-06-29 09:28:21 +0200

    Add GitHub Actions deployment

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb'],
-  base: '/3D_portfolio-main/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-  },
+  base: '/3D_portfolio-main/'
 })
